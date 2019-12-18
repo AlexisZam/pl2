b6b76176c29136a3b2d4203139a02a432e2a9ff1
Alexis Zamanis <alexiszamanis1997@gmail.com>
2019-12-18 23:49:37 +0200

    major improvements

--- lab3/impure/app/Main.hs
+++ lab3/impure/app/Main.hs
@@ -2,22 +2,19 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction -fwarn-unused-do-bind #-}
 {-# OPTIONS_GHC -O2 #-}
 {-# OPTIONS_GHC -threaded -rtsopts -with-rtsopts=-N #-}
+{-# LANGUAGE BangPatterns #-}
 
 import Control.Monad
 import Data.Array.Base
 import Data.Array.IO
--- import Data.List
--- import System.IO
 
 main :: IO ()
 main = do
-    -- parse input
     line <- getLine
     let m = (read $ last $ words line) :: Int
     contents <- getContents
     let queries = (map (map read) $ map words $ lines contents) :: [[Int]]
 
-    -- create and fill answer array
     let max' = maximum $ map last queries
     let streaks = takeWhile (<= max') $ map (subtract 1) $ iterate (* 2) 2
     a <- newArray (0, max') 0 :: IO (IOUArray Int Int)
@@ -27,19 +24,13 @@ main = do
         forM_ streaks' $ \streak -> do
             e1 <- unsafeRead a i
             e2 <- unsafeRead a streak
-            unsafeWrite a i ((e1 + e2) `mod` m)
-        
-        -- let sumMod xs = foldl' (\x y -> (x + y) `mod` m) 0 xs
-        -- es <- mapM (readArray a) streaks'
-        -- writeArray a i $ sum es `mod` m
+            unsafeWrite a i $ (e1 + e2) `mod` m
 
     forM_ [1..max'] $ \i -> do
         e1 <- unsafeRead a (i - 1)
         e2 <- unsafeRead a i
-        unsafeWrite a i ((e1 + (e2 * 2 `mod` m)) `mod` m)
+        unsafeWrite a i $ (e1 + (e2 * 2 `mod` m)) `mod` m
 
-    -- answer queries
-    -- hSetBuffering stdout (BlockBuffering (Just n))
     forM_ queries $ \[i, j] -> case i of
         0 -> unsafeRead a j >>= print
         _ -> do
--- lab3/pure/app/Main.hs
+++ lab3/pure/app/Main.hs
@@ -2,11 +2,12 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction -fwarn-unused-do-bind #-}
 {-# OPTIONS_GHC -O2 #-}
 {-# OPTIONS_GHC -threaded -rtsopts -with-rtsopts=-N #-}
+{-# LANGUAGE BangPatterns #-}
 
 import qualified Data.IntMap.Strict as IntMap -- HashMap
--- import Data.Array.IArray
--- import Data.Array.Unboxed
-import Data.List (foldl1')
+import Data.Array.IArray
+import Data.Array.Unboxed
+import Data.List (foldl1', scanl1)
 
 main :: IO ()
 main = do
@@ -23,20 +24,17 @@ answer m queries = map answer' queries
         max' = maximum $ map last queries
         streaks = takeWhile (<= max') $ map (subtract 1) $ iterate (* 2) 2
 
-        aux k acc
-            | k <= max' =
-                let ks = map (k -) $ filter (<= k) streaks
-                    sumMod xs = foldl1' (\x y -> (x + y) `mod` m) xs
-                in aux (k + 1) $ IntMap.insert k (sumMod (map (acc IntMap.!) ks)) acc
-            | otherwise = acc
+        arr :: Array Int Int
+        arr = array (0, max') ((0, 1) : [(i, foo i) | i <- [1..max']])
 
-        aux' k acc
-            | k <= max' = aux' (k + 1) $ IntMap.adjust (\x -> ((acc IntMap.! (k - 1)) + (x * 2 `mod` m)) `mod` m) k acc
-            | otherwise = acc
+        foo i = sumMod $ map (arr !) streaks'
+            where
+                streaks' = map (i -) $ filter (<= i) streaks
+                sumMod = foldl1' (\x y -> (x + y) `mod` m)
 
-        acc' = aux' 1 $ aux 1 $ IntMap.singleton 0 1
+        arr' :: UArray Int Int
+        arr' = listArray (0, max') $ 1 : (tail $ scanl1 (\x y -> ((x + (y * 2 `mod` m)) `mod` m)) (elems arr))
 
-        answer' [0, b] = acc' IntMap.! b
-        answer' [a, b] = (acc' IntMap.! b - acc' IntMap.! (a - 1)) `mod` m
+        answer' [0, b] = arr' ! b
+        answer' [a, b] = (arr' ! b - arr' ! (a - 1)) `mod` m
         answer' _ = undefined
-        
