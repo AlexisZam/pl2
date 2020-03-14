27b75716ef161f6013ec8ecd31d19143cf8fe3a7
Alexis Zamanis <alexiszamanis1997@gmail.com>
2020-03-14 18:15:29 +0200

    major changes

--- lab6/parhaskell.hs
+++ lab6/parhaskell.hs
@@ -11,39 +11,39 @@ main = do
   sequence $ take t $ repeat $ do
     line' <- getLine
     let [n, k, p] = map read $ words line' :: [Int]
-    print $ choose'' p n k
+    print $ chooseMod'' p n k
 
 -- Pascal's triangle
 
-choose :: Int -> Int -> Int -> Int
-choose p = go
+chooseMod :: Int -> Int -> Int -> Int
+chooseMod p = go
   where _ `go` 0 = 1
         0 `go` _ = 0
         n `go` k = (((n - 1) `go` (k - 1)) `mod` p + ((n - 1) `go` k) `mod` p) `mod` p
 
 -- Fermat's little theorem
 
-modExp :: Int -> Int -> Int -> Int
-modExp p = go
+expMod :: Int -> Int -> Int -> Int
+expMod p = go
   where _ `go` 0 = 1
         x `go` n = let t = x `go` (n `div` 2) ^ (2 :: Int) `mod` p
                    in if odd n then t * x `mod` p else t
 
-modProd :: Int -> [Int] -> Int
-modProd p = go
+prodMod :: Int -> [Int] -> Int
+prodMod p = go
   where go [] = 1
         go (x : xs) = (x * go xs) `mod` p
 
-modFact :: Int -> Int -> Int
-modFact p n = modProd p [1 .. n]
+factMod :: Int -> Int -> Int
+factMod p n = prodMod p [1 .. n]
 
-modInv :: Int -> Int -> Int
-modInv p x = modExp p x (p - 2)
+invMod :: Int -> Int -> Int
+invMod p x = expMod p x (p - 2)
 
-choose' :: Int -> Int -> Int -> Int
-choose' p n k = let num = modProd p [n - k + 1 .. n]
-                    den = modFact p k
-                in num * modInv p den `mod` p
+chooseMod' :: Int -> Int -> Int -> Int
+chooseMod' p n k = let num = prodMod p [n - k + 1 .. n]
+                    den = factMod p k
+                in num * invMod p den `mod` p
 
 -- Lucas's theorem (and Fermat's little theorem)
 
@@ -52,21 +52,21 @@ digitize p = go
   where go 0 = []
         go n = (n `mod` p) : go (n `div` p)
 
-choose'' :: Int -> Int -> Int -> Int
-choose'' p n k = let ns = digitize p n
+chooseMod'' :: Int -> Int -> Int -> Int
+chooseMod'' p n k = let ns = digitize p n
                      ks = digitize p k
-                 in modProd p (zipWith (choose' p) ns ks)
+                 in prodMod p (zipWith (chooseMod' p) ns ks)
 
-choosePar :: Int -> Int -> Int -> Int
-choosePar p n k = let ns = digitize p n
+chooseModPar :: Int -> Int -> Int -> Int
+chooseModPar p n k = let ns = digitize p n
                       ks = digitize p k
                   in runPar $ do
                     i <- new
                     j <- new
                     let (ns1, ns2) = splitAt (length ns `div` 2) ns
                     let (ks1, ks2) = splitAt (length ks `div` 2) ks
-                    fork (put i (modProd p (zipWith (choose' p) ns1 ks1)))
-                    fork (put j (modProd p (zipWith (choose' p) ns2 ks2)))
+                    fork (put i (prodMod p (zipWith (chooseMod' p) ns1 ks1)))
+                    fork (put j (prodMod p (zipWith (chooseMod' p) ns2 ks2)))
                     a <- get i
                     b <- get j
                     return (a * b `mod` p)
