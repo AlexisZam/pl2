de8277681d26ee3d2e8e5d9257b14a3932f30067
alexiszam <alexiszamanis1997@gmail.com>
2020-04-21 19:39:52 +0300

    fix

--- lab8/src/densem.hs
+++ lab8/src/densem.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -O2 #-}
 {-# OPTIONS_GHC -Wall -fwarn-tabs -fwarn-incomplete-record-updates -fwarn-monomorphism-restriction -fwarn-unused-do-bind #-}
 
-import Control.Monad.Fix
+import Control.Monad.Fix (fix)
 
 -- Syntax
 
@@ -39,7 +39,9 @@ sem Pfalse s = VB False : s
 sem Padd (VI n2 : VI n1 : s) = VI (n1 + n2) : s
 sem Pneg (VI n : s) = VI (- n) : s
 sem Pmul (VI n2 : VI n1 : s) = VI (n1 * n2) : s
-sem Pdiv (VI n2 : VI n1 : s) | n2 /= 0 = VI (n1 `mod` n2) : VI (n1 `div` n2) : s
+sem Pdiv (VI n2 : VI n1 : s) | n2 /= 0 = VI r : VI q : s
+  where
+    (q, r) = quotRem n1 n2
 sem Plt (VI n2 : VI n1 : s) = VB (n1 < n2) : s
 sem Peq (VI n2 : VI n1 : s) = VB (n1 == n2) : s
 sem Pand (VB b2 : VB b1 : s) = VB (b1 && b2) : s
@@ -69,7 +71,7 @@ main = do
 -- Pretty-printing
 
 instance Show P where
-  showsPrec _ (Pint n) = showsPrec 0 n
+  showsPrec _ (Pint n) = shows n
   showsPrec _ Ptrue = ("true" ++)
   showsPrec _ Pfalse = ("false" ++)
   showsPrec _ Padd = ("+" ++)
@@ -86,10 +88,10 @@ instance Show P where
   showsPrec _ Pswap = ("swap" ++)
   showsPrec _ Pswap2 = ("swap2" ++)
   showsPrec d (Pseq p1 p2) =
-    showParen (d > 0) $ showsPrec 1 p1 . (" " ++) . showsPrec 0 p2
+    showParen (d > 0) $ showsPrec 1 p1 . (" " ++) . shows p2
   showsPrec _ (Pcond p1 p2) =
-    ("cond [" ++) . showsPrec 0 p1 . (" | " ++) . showsPrec 0 p2 . ("]" ++)
-  showsPrec _ (Ploop p) = ("loop [" ++) . showsPrec 0 p . ("]" ++)
+    ("cond [" ++) . shows p1 . (" | " ++) . shows p2 . ("]" ++)
+  showsPrec _ (Ploop p) = ("loop [" ++) . shows p . ("]" ++)
 
 instance Show V where
   showsPrec d (VI n) = showsPrec d n
@@ -104,7 +106,7 @@ next _ = []
 
 instance Read P where
   readsPrec d s =
-    readParen False (\s' -> [(Pint n, r) | (n, r) <- readsPrec 0 s']) s
+    readParen False (\s' -> [(Pint n, r) | (n, r) <- reads s']) s
       ++ readParen False (\s' -> [(Ptrue, r) | ("true", r) <- lex s']) s
       ++ readParen False (\s' -> [(Pfalse, r) | ("false", r) <- lex s']) s
       ++ readParen False (\s' -> [(Padd, r) | ("+", r) <- lex s']) s
@@ -120,6 +122,6 @@ instance Read P where
       ++ readParen False (\s' -> [(Ppop, r) | ("pop", r) <- lex s']) s
       ++ readParen False (\s' -> [(Pswap, r) | ("swap", r) <- lex s']) s
       ++ readParen False (\s' -> [(Pswap2, r) | ("swap2", r) <- lex s']) s
-      ++ readParen (d > 0) (\s' -> [(Pseq p1 p2, r) | (p1, t) <- readsPrec 1 s', (p2, r) <- readsPrec 0 t]) s
-      ++ readParen False (\s' -> [(Pcond p1 p2, r) | ("cond", t1) <- lex s', ("[", t2) <- lex t1, (p1, t3) <- readsPrec 0 t2, ("|", t4) <- lex t3, (p2, t5) <- readsPrec 0 t4, ("]", r) <- lex t5]) s
-      ++ readParen False (\s' -> [(Ploop p, r) | ("loop", t1) <- lex s', ("[", t2) <- lex t1, (p, t3) <- readsPrec 0 t2, ("]", r) <- lex t3]) s
+      ++ readParen (d > 0) (\s' -> [(Pseq p1 p2, r) | (p1, t) <- readsPrec 1 s', (p2, r) <- reads t]) s
+      ++ readParen False (\s' -> [(Pcond p1 p2, r) | ("cond", t1) <- lex s', ("[", t2) <- lex t1, (p1, t3) <- reads t2, ("|", t4) <- lex t3, (p2, t5) <- reads t4, ("]", r) <- lex t5]) s
+      ++ readParen False (\s' -> [(Ploop p, r) | ("loop", t1) <- lex s', ("[", t2) <- lex t1, (p, t3) <- reads t2, ("]", r) <- lex t3]) s
