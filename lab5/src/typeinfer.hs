83092db240f1c62552aebdf9086b842521bf09d5
alexiszam <alexiszamanis1997@gmail.com>
2020-04-22 16:48:11 +0300

    fix

--- lab5/src/typeinfer.hs
+++ lab5/src/typeinfer.hs
@@ -46,37 +46,40 @@ instance Show Type where
 
 data Sub = Sub Type Type
 
-type Env = Map.Map String Type
+infer :: Expr -> Maybe Type
+infer e = do
+  let (t, subs) = derive e
+  sort . flip substitute'' t <$> unify subs
 
-constraints :: Expr -> (Type, [Sub])
-constraints e = evalState (go Map.empty e) 0
+derive :: Expr -> (Type, [Sub])
+derive e = evalState (go Map.empty e) 0
   where
-    go :: Env -> Expr -> State Int (Type, [Sub])
-    go env (Evar x) = return (env Map.! x, [])
-    go env (Eabs x e') = do
-      a <- fresh
-      (t, c) <- go (Map.insert x a env) e'
+    go :: Map.Map String Type -> Expr -> State Int (Type, [Sub])
+    go m (Evar x) = return (m Map.! x, [])
+    go m (Eabs x e') = do
+      a <- newTvar
+      (t, c) <- go (Map.insert x a m) e'
       return (Tfun a t, c)
-    go env (Eapp e1 e2) = do
-      (t1, c1) <- go env e1
-      (t2, c2) <- go env e2
-      a <- fresh
+    go m (Eapp e1 e2) = do
+      (t1, c1) <- go m e1
+      (t2, c2) <- go m e2
+      a <- newTvar
       return (a, Sub t1 (Tfun t2 a) : c1 ++ c2)
 
-fresh :: State Int Type
-fresh = do
+newTvar :: State Int Type
+newTvar = do
   i <- get
   put (i + 1)
   return (Tvar i)
 
-mgu :: [Sub] -> Maybe [Sub]
-mgu [] = Just []
-mgu (Sub t1 t2 : subs) | t1 == t2 = mgu subs
-mgu (sub@(Sub a@(Tvar _) t) : subs)
-  | a `notElem'` t = (sub :) <$> mgu (map (substitute' sub) subs)
-mgu (Sub t a@(Tvar _) : subs) = mgu (Sub a t : subs)
-mgu (Sub (Tfun t1 t2) (Tfun t1' t2') : subs) = mgu (Sub t1 t1' : Sub t2 t2' : subs)
-mgu _ = Nothing
+unify :: [Sub] -> Maybe [Sub]
+unify [] = Just []
+unify (Sub t1 t2 : subs) | t1 == t2 = unify subs
+unify (sub@(Sub a@(Tvar _) t) : subs)
+  | a `notElem'` t = (sub :) <$> unify (substitute' sub subs)
+unify (Sub t a@(Tvar _) : subs) = unify (Sub a t : subs)
+unify (Sub (Tfun t1 t2) (Tfun t1' t2') : subs) = unify (Sub t1 t1' : Sub t2 t2' : subs)
+unify _ = Nothing
 
 notElem' :: Type -> Type -> Bool
 notElem' a@(Tvar _) = go
@@ -92,16 +95,18 @@ substitute (Sub a@(Tvar _) t) = go
     go (Tfun t1 t2) = Tfun (go t1) (go t2)
 substitute _ = undefined
 
-substitute' :: Sub -> Sub -> Sub
-substitute' sub (Sub t1 t2) = Sub (substitute sub t1) (substitute sub t2)
+substitute' :: Sub -> [Sub] -> [Sub]
+substitute' sub = map go
+  where
+    go (Sub t1 t2) = Sub (substitute sub t1) (substitute sub t2)
 
 substitute'' :: [Sub] -> Type -> Type
 substitute'' subs t = if t == t' then t else substitute'' subs t'
   where
     t' = foldl' (flip substitute) t subs
 
-enumerate :: Type -> Type
-enumerate t = evalState (go t) (0, Map.empty)
+sort :: Type -> Type
+sort t = evalState (go t) (0, Map.empty)
   where
     go :: Type -> State (Int, Map.Map Int Int) Type
     go (Tvar a) = do
@@ -115,18 +120,13 @@ enumerate t = evalState (go t) (0, Map.empty)
       t2' <- go t2
       return (Tfun t1' t2')
 
-inferType :: Expr -> Maybe Type
-inferType e = do
-  let (t, subs) = constraints e
-  enumerate . flip substitute'' t <$> mgu subs
-
 -- Main program
 
 readOne :: IO ()
 readOne = do
   s <- getLine
   let e = read s :: Expr
-  maybe (putStrLn "type error") print (inferType e)
+  maybe (putStrLn "type error") print (infer e)
 
 main :: IO ()
 main = do
