dc6d0dc3542df4321ca4c02a5f5060f12d6bd719
alexiszam <alexiszamanis1997@gmail.com>
2020-04-30 11:33:26 +0300

    fix

--- lab1/scripts/test.sh
+++ lab1/scripts/test.sh
@@ -3,16 +3,19 @@ PL2_DIR=${HOME}/Workspace/pl2
 
 cd ${PL2_DIR}/lab1/src
 make >/dev/null
-for test in $(ls ../eg/*.bf); do
+for test in $(ls ../eg/~/d*.bf); do
+    input=${test%.*}.in
+    [ -f ${input} ] || input=/dev/stdin
     echo $(basename ${test})
     echo "  bef"
-    ${BEF_DIR}/bin/bef -s bef.stack ${test} >bef.out 2>bef.err
+    ${BEF_DIR}/bin/bef -s bef.stack ${test} <${input} >bef.out 2>bef.err
     echo "  vm"
-    ./vm ${test} >vm.out 2>vm.err
+    ./vm ${test} <${input} >vm.out 2>vm.err
     sed -i 1d bef.out
-    diff -q bef.out vm.out
-    diff -q bef.err vm.err
-    diff -q bef.stack vm.stack
+    diff -q *.out
+    [ $? -eq 0 ] || exit 1
+    diff -q *.err
+    diff -q *.stack
 done
 rm -f *out *err *stack
 make clean >/dev/null
