870c4cba894df7541555a91d3fb3304b976c581b
alexiszam <alexiszamanis1997@gmail.com>
2020-05-02 19:00:46 +0300

    final

--- lab2/src/gc.c
+++ lab2/src/gc.c
@@ -2,22 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
-#define HEIGHT 25
-#define WIDTH 80
+#define PROGRAM_HEIGHT 25
+#define PROGRAM_WIDTH 80
 #define STACK_SIZE (1024 * 1024)
 #define HEAP_SIZE (16 * 1024 * 1024)
 
-#define MOVE() \
-    move();    \
-    goto **pc;
-
 /* Program */
 
-char program[HEIGHT][WIDTH];
+char program[PROGRAM_HEIGHT][PROGRAM_WIDTH];
 struct {
     int x, y;
 } position = {0, 0};
@@ -26,38 +21,37 @@ struct {
 } direction = {1, 0};
 void **pc;
 
-void move() {
-    int x = position.x, y = position.y;
-    position.x = (position.x + direction.x + WIDTH) % WIDTH;
-    position.y = (position.y + direction.y + HEIGHT) % HEIGHT;
-    pc += (position.y - y) * WIDTH + (position.x - x);
-}
-
 void init_program() {
-    for (int y = 0; y < HEIGHT; y++)
-        for (int x = 0; x < WIDTH; x++)
-            program[y][x] = ' ';
+    memset(program, ' ', PROGRAM_HEIGHT * PROGRAM_WIDTH);
 }
 
-void read_program(const char *filename) {
-    FILE *stream = fopen(filename, "r");
+void read_program(const char *pathname) {
+    FILE *stream = fopen(pathname, "r");
     if (!stream) {
-        printf("Error: couldn't open '%s' for input.\n", filename);
+        printf("Error: couldn't open '%s' for input.\n", pathname);
         exit(EXIT_FAILURE);
     }
     char *lineptr = NULL;
     size_t n = 0;
-    for (int y = 0; y < HEIGHT; y++) {
-        ssize_t cnt = getline(&lineptr, &n, stream);
-        if (cnt == -1)
+    for (int y = 0; y < PROGRAM_HEIGHT; y++) {
+        ssize_t ssize = getline(&lineptr, &n, stream);
+        if (ssize == -1)
             break;
-        if (lineptr[cnt - 1] == '\n')
-            cnt--;
-        if (cnt > WIDTH)
-            cnt = WIDTH;
-        memcpy(program[y], lineptr, cnt);
+        if (lineptr[ssize - 1] == '\n')
+            ssize--;
+        if (ssize > PROGRAM_WIDTH)
+            ssize = PROGRAM_WIDTH;
+        memcpy(program[y], lineptr, ssize);
     }
+    free(lineptr);
     fclose(stream);
+}
+
+void move() {
+    int x = position.x, y = position.y;
+    position.x = (position.x + direction.x + PROGRAM_WIDTH) % PROGRAM_WIDTH;
+    position.y = (position.y + direction.y + PROGRAM_HEIGHT) % PROGRAM_HEIGHT;
+    pc += (position.y - y) * PROGRAM_WIDTH + (position.x - x);
 }
 
 /* Stack */
@@ -71,10 +65,6 @@ struct value stack[STACK_SIZE];
 int top = 0;
 
 void push(long l) {
-    if (top == STACK_SIZE) {
-        fprintf(stderr, "Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
     stack[top++] = (struct value){l, false, false};
 }
 
@@ -84,17 +74,13 @@ long pop() {
     return stack[--top].value;
 }
 
-void push_value(struct value value) {
-    if (top == STACK_SIZE) {
-        fprintf(stderr, "Stack overflow\n");
-        exit(EXIT_FAILURE);
-    }
-    stack[top++] = value;
+void push_value(struct value v) {
+    stack[top++] = v;
 }
 
 struct value pop_value() {
     if (!top)
-        return (struct value){0};
+        return (struct value){0, false, false};
     return stack[--top];
 }
 
@@ -104,7 +90,6 @@ struct cons_cell {
     struct value head;
     struct value tail;
 };
-
 struct cons_cell heap[HEAP_SIZE];
 int freelist = 0;
 
@@ -115,12 +100,12 @@ void init_heap() {
 
 /* GC */
 
-void DFS(struct value *x) { // TODO: improve DFS
-    if (!x->marked) {
-        x->marked = true;
-        if (x->pointer) {
-            DFS(&heap[x->value].head);
-            DFS(&heap[x->value].tail);
+void dfs(struct value v) {
+    if (v.pointer) {
+        if (!heap[v.value].head.marked) {
+            heap[v.value].head.marked = true;
+            dfs(heap[v.value].head);
+            dfs(heap[v.value].tail);
         }
     }
 }
@@ -128,12 +113,11 @@ void DFS(struct value *x) { // TODO: improve DFS
 void gc() {
     /* mark */
     for (int i = 0; i < top; i++)
-        if (stack[i].pointer)
-            DFS(&stack[i]);
+        dfs(stack[i]);
     /* sweep */
     for (int i = HEAP_SIZE - 1; i >= 0; i--) {
         if (heap[i].head.marked)
-            heap[i].head.marked = heap[i].tail.marked = false;
+            heap[i].head.marked = false;
         else {
             heap[i].head.value = freelist;
             freelist = i;
@@ -142,19 +126,19 @@ void gc() {
 }
 
 int main(int argc, char *argv[]) {
-    long l, l1, l2;
-    struct value v, v1, v2;
-
     if (argc != 2) {
-        printf("Usage: vm foo.bf\n");
+        printf("USAGE: vm foo.bf\n");
         exit(EXIT_FAILURE);
     }
 
     init_program();
     read_program(argv[1]);
 
-#define ASCII 128
-    void *labels[] = {[0 ... ASCII - 1] = &&unsupported};
+    init_heap();
+
+    srand(time(NULL));
+
+    void *labels[] = {[0 ... 127] = &&nop};
     for (char c = '0'; c <= '9'; c++)
         labels[c] = &&digit;
     labels['+'] = &&add;
@@ -188,73 +172,71 @@ int main(int argc, char *argv[]) {
     labels['h'] = &&head;
     labels['t'] = &&tail;
 
-    void *program_as_labels[HEIGHT][WIDTH];
-    for (int j = 0; j < HEIGHT; j++)
-        for (int i = 0; i < WIDTH; i++)
-            program_as_labels[j][i] = labels[program[j][i]];
-    pc = program_as_labels[0];
+    void *program_as_labels[PROGRAM_HEIGHT][PROGRAM_WIDTH];
+    for (int y = 0; y < PROGRAM_HEIGHT; y++)
+        for (int x = 0; x < PROGRAM_WIDTH; x++)
+            program_as_labels[y][x] = labels[program[y][x]];
 
-    srand(time(NULL));
-
-    init_heap();
-
+    long l, l1, l2;
+    struct value v, v1, v2;
+    pc = *program_as_labels;
     goto **pc;
 digit:
     push(program[position.y][position.x] - '0');
-    MOVE()
+    move();
+    goto **pc;
 add:
     push(pop() + pop());
-    MOVE()
+    move();
+    goto **pc;
 subtract:
     l = pop();
     push(pop() - l);
-    MOVE()
+    move();
+    goto **pc;
 multiply:
     push(pop() * pop());
-    MOVE()
+    move();
+    goto **pc;
 divide:
     l = pop();
-    if (l == 0) {
-        pop();
-        printf("What do you want %ld/0 to be? ", l);
-        scanf("%ld", &l);
-        push(l);
-    } else
-        push(pop() / l);
-    MOVE()
+    push(pop() / l);
+    move();
+    goto **pc;
 modulo:
     l = pop();
-    if (l == 0) {
-        pop();
-        printf("What do you want %ld/0 to be? ", l);
-        scanf("%ld", &l);
-        push(l);
-    } else
-        push(pop() % l);
-    MOVE()
+    push(pop() % l);
+    move();
+    goto **pc;
 negate:
     push(!pop());
-    MOVE()
+    move();
+    goto **pc;
 greater:
     l = pop();
     push(pop() > l);
-    MOVE()
+    move();
+    goto **pc;
 right:
     direction.x = 1;
     direction.y = 0;
-    MOVE()
+    move();
+    goto **pc;
 left:
     direction.x = -1;
     direction.y = 0;
-    MOVE()
+    move();
+    goto **pc;
 up:
     direction.x = 0;
     direction.y = -1;
-    MOVE()
+    move();
+    goto **pc;
 down:
     direction.x = 0;
     direction.y = 1;
-    MOVE()
+    move();
+    goto **pc;
 random:
     switch (rand() % 4) {
     case 0:
@@ -262,9 +244,9 @@ random:
     case 1:
         goto left;
     case 2:
-        goto down;
-    case 3:
         goto up;
+    case 3:
+        goto down;
     }
 horizontal_if:
     if (pop())
@@ -281,84 +263,90 @@ stringmode:
             break;
         push(program[position.y][position.x]);
     }
-    MOVE()
+    move();
+    goto **pc;
 dup:
     v = pop_value();
     push_value(v);
     push_value(v);
-    MOVE()
+    move();
+    goto **pc;
 swap:
     v1 = pop_value();
     v2 = pop_value();
     push_value(v1);
     push_value(v2);
-    MOVE()
+    move();
+    goto **pc;
 pop:
     pop();
-    MOVE()
+    move();
+    goto **pc;
 output_int:
     printf("%ld ", pop());
     fflush(stdout);
-    MOVE()
+    move();
+    goto **pc;
 output_char:
     printf("%c", (char)pop());
     fflush(stdout);
-    MOVE()
+    move();
+    goto **pc;
 bridge:
     move();
-    MOVE()
+    move();
+    goto **pc;
 get:
     l1 = pop();
     l2 = pop();
-    if (l1 < 0 || l1 >= HEIGHT || l2 < 0 || l2 >= WIDTH) {
-        fprintf(stderr, "g 'Get' instruction out of bounds (%ld,%ld)\n", l2, l1);
+    if (l1 < 0 || l1 >= PROGRAM_HEIGHT || l2 < 0 || l2 >= PROGRAM_WIDTH)
         push(0);
-    } else
+    else
         push(program[l1][l2]);
-    MOVE()
+    move();
+    goto **pc;
 put:
     l1 = pop();
     l2 = pop();
-    if (l1 < 0 || l1 >= HEIGHT || l2 < 0 || l2 >= WIDTH) {
-        fprintf(stderr, "p 'Put' instruction out of bounds (%ld,%ld)\n", l2, l1);
+    if (l1 < 0 || l1 >= PROGRAM_HEIGHT || l2 < 0 || l2 >= PROGRAM_WIDTH)
         pop();
-    } else {
+    else {
         program[l1][l2] = pop();
         program_as_labels[l1][l2] = labels[program[l1][l2]];
     }
-    MOVE()
+    move();
+    goto **pc;
 input_int:
     l = -1;
     scanf("%ld", &l);
     push(l);
-    MOVE()
+    move();
+    goto **pc;
 input_char:
     push(getchar());
-    MOVE()
+    move();
+    goto **pc;
 end:
     exit(EXIT_SUCCESS);
 nop:
-    MOVE()
+    move();
+    goto **pc;
 cons:
-    if (freelist == HEAP_SIZE) {
+    if (freelist == HEAP_SIZE)
         gc();
-        if (freelist == HEAP_SIZE) {
-            fprintf(stderr, "Heap overflow\n");
-            exit(EXIT_FAILURE);
-        }
-    }
+    l = heap[freelist].head.value;
     v = pop_value();
     heap[freelist] = (struct cons_cell){pop_value(), v};
     push_value((struct value){freelist, true, false});
-    freelist = heap[freelist].head.value;
-    MOVE()
+    freelist = l;
+    move();
+    goto **pc;
 head:
     push_value(heap[pop()].head);
-    MOVE()
+    move();
+    goto **pc;
 tail:
     push_value(heap[pop()].tail);
-    MOVE()
-unsupported:
-    fprintf(stderr, "Unsupported instruction '%c' (0x%02x) (maybe not Befunge-93?)\n", program[position.y][position.x], program[position.y][position.x]);
-    MOVE()
+    move();
+    goto **pc;
 }
