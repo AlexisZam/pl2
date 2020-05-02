28095df1252d409bccde0c91e17e11b0dbd6a00e
alexiszam <alexiszamanis1997@gmail.com>
2020-05-02 21:44:27 +0300

    fix

--- lab10/src/samenum.c
+++ lab10/src/samenum.c
@@ -1,43 +1,30 @@
 #define MAXN 1000000
 #define MAXV 2000000
 
-/*@ predicate samenum(int N, int *x, int r) =
-  @   \exists integer i, j; x[i] == x[j] && i < j && r == j - i;
+/*@ predicate foo(int N, int *x, int r) = \exists int i, j; x[i] == x[j] && i < j && r == j - i;
   @*/
 
-/*@ predicate best(int N, int *x, int r) =
-  @   samenum(N, x, r) && \forall int rr; samenum(N, x, rr) ==> r >= rr;
+/*@ predicate bar(int N, int *x, int r) = foo(N, x, r) && \forall int rr; foo(N, x, rr) ==> r >= rr;
   @*/
-
-// ensures \result != 0 <==> \exists int i, j;
-//          x[i] == x[j] && i < j && \result == j - i && \forall int ii, jj;
-//          x[i] != x[ii] && x[ii] == x[jj] && ii < jj ==> \result >= jj - ii;
 
 /*@ requires 2 <= N <= MAXN;
   @ requires \valid_read(x + (0 .. N - 1));
   @ requires \forall int i; 0 <= i < N ==> 1 <= x[i] <= MAXV;
-  @ assigns \nothing;
-  @ ensures best(N, x, \result);
+  @ ensures \result != 0 <==> bar(N, x, \result);
   @*/
 int samenum(int N, int *x) {
-    /*@ requires \valid(p + (0 .. MAXV));
-      @ requires \forall int i; 0 <= i <= MAXV ==> 0 <= p[i] <= MAXN;
-      @*/
     int p[MAXV + 1];
-    /*@ loop invariant 0 <= i <= MAXV + 1;
-      @ loop assigns i, p[0 .. MAXV];
+    /*@ loop assigns i, p[0 .. MAXV];
+      @ loop invariant 0 <= i <= MAXV + 1;
+      @ loop invariant \forall int j; 0 <= j < i ==> p[j] == -1;
       @ loop variant MAXV - i;
       @*/
     for (int i = 0; i <= MAXV; ++i)
         p[i] = -1;
-    //@ assert \forall int i; 0 <= i <= MAXV ==> p[i] == -1;
     int best = 0;
-    //@ assert best == 0;
-    /*@ loop invariant 0 <= i <= N;
-      @ loop invariant \forall int i; 0 <= i < N ==> best >= \at(best, Pre);
-      @ loop invariant \forall int i; 0 <= i < N ==> p[i] >= \at(p[i], Pre);
-      @ loop invariant \forall int i; 0 <= i < N ==> best >= i - p[x[i]];
-      @ loop assigns i, p[0 .. N - 1], best;
+    /*@ loop assigns best, i, p[0 .. MAXV];
+      @ loop invariant 0 <= i <= N;
+      @ loop invariant \forall int j; 0 <= j < i ==> p[x[j]] == j || best >= j - p[x[j]];
       @ loop variant N - 1 - i;
       @*/
     for (int i = 0; i < N; ++i)
