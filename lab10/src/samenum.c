af56de73c44d3e8e520f3be1f94492bec430d53a
alexiszam <alexiszamanis1997@gmail.com>
2020-05-03 10:36:32 +0300

    added eg

--- lab10/src/samenum.c
+++ lab10/src/samenum.c
@@ -1,31 +1,32 @@
 #define MAXN 1000000
 #define MAXV 2000000
 
-/*@ predicate foo(int N, int *x, int r) = \exists int i, j; x[i] == x[j] && i < j && r == j - i;
+/*@ predicate samenum(int N, int *x, int r) =
+  @   \exists int i, j; x[i] == x[j] && i < j && r == j - i &&
+  @     \forall int ii, jj; ii != i && jj != j && x[ii] == x[jj] && ii < jj ==> r >= jj - ii;
   @*/
 
-/*@ predicate bar(int N, int *x, int r) = foo(N, x, r) && \forall int rr; foo(N, x, rr) ==> r >= rr;
-  @*/
-
-/*@ requires 2 <= N <= MAXN;
-  @ requires \valid_read(x + (0 .. N - 1));
-  @ requires \forall int i; 0 <= i < N ==> 1 <= x[i] <= MAXV;
-  @ ensures \result != 0 <==> bar(N, x, \result);
+/*@ requires  2 <= N <= MAXN;
+  @ requires  \valid_read(x + (0 .. N - 1));
+  @ requires  \forall int i; 0 <= i < N ==> 1 <= x[i] <= MAXV;
+  @ ensures   \result != 0 <==> samenum(N, x, \result);
   @*/
 int samenum(int N, int *x) {
     int p[MAXV + 1];
-    /*@ loop assigns i, p[0 .. MAXV];
-      @ loop invariant 0 <= i <= MAXV + 1;
-      @ loop invariant \forall int j; 0 <= j < i ==> p[j] == -1;
-      @ loop variant MAXV - i;
+    /*@ loop assigns    i, p[0 .. MAXV];
+      @ loop invariant  0 <= i <= MAXV + 1;
+      @ loop invariant  \forall int j; 0 <= j < i ==> p[j] == -1;
+      @ loop variant    MAXV - i;
       @*/
     for (int i = 0; i <= MAXV; ++i)
         p[i] = -1;
     int best = 0;
-    /*@ loop assigns best, i, p[0 .. MAXV];
-      @ loop invariant 0 <= i <= N;
-      @ loop invariant \forall int j; 0 <= j < i ==> p[x[j]] == j || best >= j - p[x[j]];
-      @ loop variant N - 1 - i;
+    /*@ loop assigns    best, i, p[0 .. MAXV];
+      @ loop invariant  0 <= i <= N;
+      @ loop invariant  best == 0 || \exists int j; 0 <= j < i && best >= j - p[x[j]];
+      @ loop invariant  \forall int j; 0 <= j < i ==> p[x[j]] == j || best >= j - p[x[j]];
+      @ loop invariant  samenum(i, x, best);
+      @ loop variant    N - 1 - i;
       @*/
     for (int i = 0; i < N; ++i)
         if (p[x[i]] == -1)
